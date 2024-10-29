5df911b05a2f259ee9fd3ec6420a2bbd24794c7c
MathAff <158769872+MathAff@users.noreply.github.com>
2024-10-28 22:40:52 -0300

    Making the connection credencials hidden from github

--- website/php/utils/ConnectionDB.php
+++ website/php/utils/ConnectionDB.php
@@ -1,21 +1,29 @@
 <?php
 namespace imobiliariati\utils;
 
+require_once 'autoload.php';
+
 use PDO;
 use PDOException;
+use imobiliariati\utils\ConnectionCredencials;
 
 class ConnectionDB {
+    private $db = "imobiliariati";
+    private $user = "imobiliariati";
+    private $psw = "1f7.2;C1Kj7;";
+    private $server = "db4free.net";
+
     public static $conn = null;
     
     public static function connect () {
-        $db = "imobiliariati";
-        $user = "imobiliariati";
-        $psw = "1f7.2;C1Kj7;";
-        $server = "db4free.net";
+        self::$db = ConnectionCredencials::getDb();
+        self::$user = ConnectionCredencials::getUser();
+        self::$psw = ConnectionCredencials::getPsw();
+        self::$server = ConnectionCredencials::getServer();
 
         try {
 
-            self::$conn = new PDO ('mysql:host='.$server.';dbname='.$db, $user, $psw);
+            self::$conn = new PDO ('mysql:host='.self::$server.';dbname='.self::$db, self::$user, self::$psw);
             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return self::$conn;
 
